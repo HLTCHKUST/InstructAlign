8f8a1d8c4a513b949bee51cc8cee0fbd197d2b2e
scahyawijaya <scahyawijaya@connect.ust.hk>
2023-04-14 17:05:25 +0800

    remove select 100 samples dummy from run_t2t_finetuning

--- run_t2t_finetuning.py
+++ run_t2t_finetuning.py
@@ -359,8 +359,8 @@ def main():
             model_inputs = tokenizer(inputs, max_length=data_args.max_source_length, padding=False, truncation=True)
         return model_inputs
 
-    train_dataset = raw_datasets["train"].select([i for i in range(100)])
-    eval_dataset = raw_datasets["test"].select([i for i in range(100)])
+    train_dataset = raw_datasets["train"] # .select([i for i in range(100)])
+    eval_dataset = raw_datasets["test"] # .select([i for i in range(100)])
             
     train_dataset.set_transform(preprocess_fn)
     eval_dataset.set_transform(preprocess_fn)
